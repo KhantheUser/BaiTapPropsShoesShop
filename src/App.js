500c6fac657b150f25dd6a8b507d34a794f27575
Thien Duc <ducthiennguyen1601@gmail.com>
2022-06-03 23:42:20 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import ShoesShop from './ShoesShop/ShoesShop';
+import BaiTapXucXac from './BaiTapXucXac/BaiTapXucXac';
 
 function App() {
   return (
     <div className="App">
-      <ShoesShop/>
+      {/* <ShoesShop/> */}
+      <BaiTapXucXac/>
     </div>
   );
 }
